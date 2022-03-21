f3725f22033870e9ca744d1ce8e9703a2e5a3c54
Wilson Moura <wilo.moura3@gmail.com>
2022-03-21 15:02:31 -0300

    Corrections in implements JWTAuth

--- routes/api.php
+++ routes/api.php
@@ -14,14 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('login', 'AuthController@login');
+Route::post('login', [App\Http\Controllers\AuthController::class, 'login']);
 
-Route::group(['middleware' => 'api', 'prefix' => 'auth'], function () {
+Route::group(['middleware' => 'jwt'], function () {
 
     Route::prefix('user')->group(function() {
-        Route::get('me', 'AuthController@me');
+        Route::get('me', [App\Http\Controllers\AuthController::class, 'me']);
         
-        Route::post('refresh', 'AuthController@refresh');
-        Route::post('logout', 'AuthController@logout');
+        Route::post('refresh', [App\Http\Controllers\AuthController::class, 'refresh']);
+        Route::post('logout', [App\Http\Controllers\AuthController::class, 'logout']);
     });
 });
